3ca35c9cbb38e393c41f7619fa835e634208734f
Austin-Engstrom <aengstrom@eshipping.biz>
2026-08-02 21:50:10 -0500

    name update in __init__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,6 @@
 from src.models.activity import Activity
 from src.models.base import Base
 from src.models.gear import Gear
-from src.models.activity_streams import ActivityStream
+from src.models.activity_stream import ActivityStream
 
 __all__ = ["Activity", "Base", "Gear", "ActivityStream"]
